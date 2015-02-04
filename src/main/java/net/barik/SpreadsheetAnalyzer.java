33818382946a143ad11c3b6091875c3007a7b796
Kevin Lubick <kjlubick@ncsu.edu>
2015-02-04 11:59:52 -0500

    Adds test for Int input cells

--- src/main/java/net/barik/SpreadsheetAnalyzer.java
+++ src/main/java/net/barik/SpreadsheetAnalyzer.java
@@ -1,16 +1,50 @@
 package net.barik;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 
-public class SpreadsheetAnalyzer {
+import net.barik.SpreadsheetAnalyzer.InputCellType;
 
-	public static SpreadsheetAnalyzer parse(InputStream is) {
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class SpreadsheetAnalyzer {
+	
+	public enum InputCellType {
+		INTEGER,BOOLEAN,DATE,ERROR,NON_INTEGER_NUMBER,STRING
+	}
+
+	private Workbook workbook;
+
+	private SpreadsheetAnalyzer(Workbook wb) {
+		this.workbook = wb;
+	}
+
+	public static SpreadsheetAnalyzer parse(InputStream is) throws InvalidFormatException, IOException {
+		SpreadsheetAnalyzer analyzer = new SpreadsheetAnalyzer(WorkbookFactory.create(is));
+		
+		analyzer.analyze();
+
+		return analyzer;
+	}
+
+
+	private void analyze() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Map<String, Integer> getFunctionCounts() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public Map<String, Integer> getFunctionCounts() {
+	public Map<InputCellType, Integer> getInputCellCounts() {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- src/test/java/net/barik/TestSpreadsheetAnalyzer.java
+++ src/test/java/net/barik/TestSpreadsheetAnalyzer.java
@@ -1,30 +1,44 @@
 package net.barik;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 import java.io.InputStream;
 import java.util.Map;
 
+import net.barik.SpreadsheetAnalyzer.InputCellType;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestSpreadsheetAnalyzer {
+	
+	private static SpreadsheetAnalyzer analyzer;
 
-	@Test
-	public void testFindWorksheet() throws Exception {
+	@BeforeClass
+	public static void loadSmallWorksheet() throws Exception{
 		InputStream is = TestSpreadsheetAnalyzer.class.getResourceAsStream("/small-worksheet.xlsx");
 		assertNotNull(is);
+		analyzer = SpreadsheetAnalyzer.parse(is);
+		assertNotNull(analyzer);
+		
 	}
+
 	
 	@Test
 	public void testCountingSum() throws Exception {
-		InputStream is = TestSpreadsheetAnalyzer.class.getResourceAsStream("/test/small-worksheet.xlsx");
-		SpreadsheetAnalyzer analyzer = SpreadsheetAnalyzer.parse(is);
-		
 		Map<String, Integer> counts = analyzer.getFunctionCounts();
 		Integer sumCounts = counts.get("SUM");
 		assertNotNull(sumCounts);
 		assertEquals(13, sumCounts.intValue());
+	}
+	
+	@Test
+	public void testCountingIntInput() throws Exception {
+		Map<SpreadsheetAnalyzer.InputCellType, Integer> counts = analyzer.getInputCellCounts();
+		Integer sumCounts = counts.get(InputCellType.INTEGER);
+		assertNotNull(sumCounts);
+		assertEquals(36, sumCounts.intValue());
+		
 	}
 
 }
