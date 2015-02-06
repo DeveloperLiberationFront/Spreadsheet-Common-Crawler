500c55cd99732126b131fb91976e3ca2f2a53eb8
Kevin Lubick <kjlubick@ncsu.edu>
2015-02-06 09:13:50 -0500

    Adds R1C1 tests
    Two of them fail, probably because
    "R1C1 is not supported, and there is no plan to add that support as yet.
    If you
    want to easily convert numeric references to A1 style references easily,
    you
    can use the AreaReference and CellReference classes in the hssf.util
    package,
    which have useful constructors, and whose toString() methods return A1
    style
    references. (remember to use zero based references)" -
    http://osdir.com/ml/jakarta.poi.user/2002-07/msg00003.html

---
+++ src/test/java/net/barik/TestEusesAnalysis_r1c1.java
@@ -0,0 +1,78 @@
+package net.barik;
+
+import static org.junit.Assert.*;
+
+import java.io.InputStream;
+import java.util.Map;
+
+import net.barik.SpreadsheetAnalyzer.FunctionEvalType;
+import net.barik.SpreadsheetAnalyzer.InputCellType;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestEusesAnalysis_r1c1 {
+
+	private static SpreadsheetAnalyzer analyzer;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		InputStream is = TestEusesAnalysis_small.class.getResourceAsStream("/r1c1.xlsx");
+		assertNotNull(is);
+		analyzer = SpreadsheetAnalyzer.doEUSESAnalysis(is);
+		assertNotNull(analyzer);
+	}
+
+	@Test
+	public void testGetFunctionCounts() {
+		Map<String, Integer> counts = analyzer.getFunctionCounts();
+		Integer count = counts.get("SUM");
+		assertNotNull(count);
+		assertEquals(1, count.intValue());
+		
+		assertTrue(counts.keySet().size() == 1);//sum is the only input
+	}
+
+	@Test
+	public void testGetInputCellCounts() {
+		Map<SpreadsheetAnalyzer.InputCellType, Integer> counts = analyzer.getInputCellCounts();
+		Integer count = counts.get(InputCellType.INTEGER);
+		assertNotNull(count);
+		assertEquals(4, count.intValue());
+
+		assertTrue(counts.keySet().size() == 1);//only integer inputs
+	}
+
+	@Test
+	public void testGetFormulaCellCounts() {
+		Map<SpreadsheetAnalyzer.FunctionEvalType, Integer> counts = analyzer.getFormulaCellCounts();
+		Integer count = counts.get(FunctionEvalType.INTEGER);
+		assertNotNull(count);
+		assertEquals(6,  count.intValue());
+		
+		assertTrue(counts.keySet().size() == 1);//only integer formula results
+	}
+
+	@Test
+	public void testGetInputReferences() {
+		Map<SpreadsheetAnalyzer.InputCellType, Integer> counts = analyzer.getInputReferences();
+		Integer count = counts.get(InputCellType.INTEGER);
+		assertNotNull(count);
+		assertEquals(4, count.intValue());
+
+		assertTrue(counts.keySet().size() == 1);//only integer inputs
+	}
+
+	@Test
+	public void testGetFormulaReferencingOtherCells() {
+		int count = analyzer.getFormulaReferencingOtherCells();
+		assertEquals(6, count);
+	}
+
+	@Test
+	public void testGetFormulasReferenced() {
+		int count = analyzer.getFormulasReferenced();
+		assertEquals(2, count);
+	}
+
+}
