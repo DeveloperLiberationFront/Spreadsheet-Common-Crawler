2aca348c95f3a19d2ebbb716e15936d79c8e50d9
Kevin Lubick <kjlubick@ncsu.edu>
2015-02-16 13:35:21 -0500

    Removes WAT references
    And other things left over from copy-pasting Titus's old code

--- spreadsheet-hadoop/src/main/java/net/barik/spreadsheet/JacksonS3Export.java
+++ spreadsheet-hadoop/src/main/java/net/barik/spreadsheet/JacksonS3Export.java
@@ -14,13 +14,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JacksonS3Export {
     public static final String BUCKET_NAME = "barik-cc";
-
-    public static void exportItems(List<WATExportModel> items) throws IOException {
-        for (WATExportModel watExportModel : items) {
-            byte[] data = serializeModel(watExportModel);
-            putObjectS3(BUCKET_NAME, getKeyForURI(watExportModel.getWarcRecordId()), data);
-        }
-    }
     
     public static void exportItem(AnalysisOutput ao) throws IOException {
             byte[] data = serializeModel(ao);
@@ -52,21 +45,4 @@ public class JacksonS3Export {
         s3client.putObject(bucket, key, is, objectMetadata);
     }
 
-    public static void main(String[] args) throws IOException {
-        WATExportModel watExportModel = new WATExportModel(
-                "common-crawl/crawl-data/CC-MAIN-2014-35/segments/" +
-                        "1408500800168.29/wat/CC-MAIN-20140820021320-00000-ip-10-180-136-8.ec2.internal.warc.wat.gz",
-                "http://digitalcommons.uconn.edu/cgi/" +
-                        "viewcontent.cgi?filename=2&article=1001&context=uchcedu_annreports&type=additional",
-                "2014-08-20T04:53:54Z",
-                "<urn:uuid:639a339b-40aa-4aca-a629-186c9adbe2a2>",
-                "<urn:uuid:41a7feb1-46aa-4d90-acb7-205851f567a3>",
-                "application/json",
-                1858,
-                "{ \"Envelope\": {} }"
-        );
-
-        byte[] data = serializeModel(watExportModel);
-        putObjectS3(BUCKET_NAME, getKeyForURI(watExportModel.getWarcRecordId()), data);
-    }
 }
--- spreadsheet-hadoop/src/main/java/net/barik/spreadsheet/S3Load.java
+++ spreadsheet-hadoop/src/main/java/net/barik/spreadsheet/S3Load.java
@@ -11,19 +11,10 @@ public class S3Load {
 
     public static InputStream loadSpreadsheet(String key) throws IOException {
         AmazonS3 s3client = new AmazonS3Client();
-        S3Object object = s3client.getObject(BUCKET_NAME, "analysis/binaries/"+key);
+        S3Object object = s3client.getObject(BUCKET_NAME, key);
         InputStream is = object.getObjectContent();
 
         return is;
     }
 
-    public static void main(String[] args) throws IOException {
-
-        System.out.println("Hello world!");
-        String path = "common-crawl/crawl-data/CC-MAIN-2014-35/segments/1408500800168.29" +
-                "/wat/CC-MAIN-20140820021320-00000-ip-10-180-136-8.ec2.internal.warc.wat.gz";
-
-        InputStream is = loadSpreadsheet(path);
-        WATParser.parse(path, is);
-    }
 }
