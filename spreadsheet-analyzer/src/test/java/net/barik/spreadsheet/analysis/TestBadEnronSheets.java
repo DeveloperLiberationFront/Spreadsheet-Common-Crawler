8f2d97b75f9ce8623fd5b6b615739044bee931d0
Kevin Lubick <kjlubick@ncsu.edu>
2015-02-17 16:55:03 -0500

    Adds "gold standard" measurements for enron sheets

--- spreadsheet-analyzer/src/test/java/net/barik/spreadsheet/analysis/TestBadEnronSheets.java
+++ spreadsheet-analyzer/src/test/java/net/barik/spreadsheet/analysis/TestBadEnronSheets.java
@@ -15,7 +15,50 @@ public class TestBadEnronSheets {
 		AnalysisOutput analysis = SpreadsheetAnalyzer.doAnalysisAndGetObject(is, "[test]", "bad_enron_1.xlsx");
 		assertNotNull(analysis);
 		assertEquals("OK", analysis.errorNotification);
-		
+		assertEquals(12646, analysis.totalInputCells);
+		assertEquals(13, analysis.booleanInputCells);
+		assertEquals(1299, analysis.dateTimeInputCells);
+		assertEquals(0, analysis.errorInputCells);
+		assertEquals(1696, analysis.integerInputCells);
+		assertEquals(9301, analysis.nonIntegerInputCells);
+		assertEquals(337, analysis.stringInputCells);
+		assertEquals(1772, analysis.totalReferencedInput);
+		assertEquals(13, analysis.booleanReferencedInput);
+		assertEquals(368, analysis.dateReferencedInput);
+		assertEquals(0, analysis.errorReferencedInput);
+		assertEquals(96, analysis.integerReferencedInput);
+		assertEquals(1271, analysis.nonIntegerReferencedInput);
+		assertEquals(24, analysis.stringReferencedInput);
+		assertEquals(5091, analysis.totalFormulas);
+		assertEquals(0, analysis.booleanFormulas);
+		assertEquals(1269, analysis.dateTimeFormulas);
+		assertEquals(1520, analysis.errorFormulas);
+		assertEquals(351, analysis.integerFormulas);
+		assertEquals(1927, analysis.nonIntegerFormulas);
+		assertEquals(24, analysis.stringFormulas);
+		assertEquals(0, analysis.blankFormulas);
+		assertEquals(4048, analysis.formulaCellsReferencingOthers);
+		assertEquals(1828, analysis.formulaCellsReferencedByOthers);
+		assertEquals(121, analysis.formulaCellsOccuringOnce);
+		assertEquals(273, analysis.formulaCellsOccuring2Plus);
+		assertEquals(21, analysis.formulaCellsOccuring5Plus);
+		assertEquals(19, analysis.formulaCellsOccuring10Plus);
+		assertEquals(19, analysis.formulaCellsOccuring25Plus);
+		assertEquals(17, analysis.formulaCellsOccuring50Plus);
+		assertEquals(11, analysis.formulaCellsOccuring100Plus);
+		assertEquals(521, analysis.mostFrequentFormulaCount);
+		assertEquals("R[0]C[19]", analysis.mostFrequentFormula);
+		assertFalse(analysis.containsMacros);
+		assertEquals(2, analysis.countCEILING);
+        assertEquals(272, analysis.countDATE);
+		assertEquals(17, analysis.countDAY);
+		assertEquals(32, analysis.countIF);
+		assertEquals(23, analysis.countMAX);
+		assertEquals(292, analysis.countMONTH);
+		assertEquals(1, analysis.countSUM);
+		assertEquals(251, analysis.countVLOOKUP);
+		assertEquals(288, analysis.countYEAR);
+		//everything else is 0
 	}
 	
 	@Test
@@ -34,6 +77,52 @@ public class TestBadEnronSheets {
 		AnalysisOutput analysis = SpreadsheetAnalyzer.doAnalysisAndGetObject(is, "[test]", "bad_enron_3.xlsx");
 		assertNotNull(analysis);
 		assertEquals("OK", analysis.errorNotification);
+		assertEquals(3503, analysis.totalInputCells);
+		assertEquals(0, analysis.booleanInputCells);
+		assertEquals(20, analysis.dateTimeInputCells);
+		assertEquals(0, analysis.errorInputCells);
+		assertEquals(1503, analysis.integerInputCells);
+		assertEquals(1239, analysis.nonIntegerInputCells);
+		assertEquals(741, analysis.stringInputCells);
+		assertEquals(1093, analysis.totalReferencedInput);
+		assertEquals(0, analysis.booleanReferencedInput);
+		assertEquals(0, analysis.dateReferencedInput);
+		assertEquals(0, analysis.errorReferencedInput);
+		assertEquals(1011, analysis.integerReferencedInput);
+		assertEquals(53, analysis.nonIntegerReferencedInput);
+		assertEquals(29, analysis.stringReferencedInput);
+		assertEquals(3298, analysis.totalFormulas);
+		assertEquals(0, analysis.booleanFormulas);
+		assertEquals(0, analysis.dateTimeFormulas);
+		assertEquals(1101, analysis.errorFormulas);
+		assertEquals(1999, analysis.integerFormulas);
+		assertEquals(102, analysis.nonIntegerFormulas);
+		assertEquals(96, analysis.stringFormulas);
+		assertEquals(0, analysis.blankFormulas);
+		assertEquals(2661, analysis.formulaCellsReferencingOthers);
+		assertEquals(2452, analysis.formulaCellsReferencedByOthers);
+		assertEquals(134, analysis.formulaCellsOccuringOnce);
+		assertEquals(111, analysis.formulaCellsOccuring2Plus);
+		assertEquals(89, analysis.formulaCellsOccuring5Plus);
+		assertEquals(80, analysis.formulaCellsOccuring10Plus);
+		assertEquals(10, analysis.formulaCellsOccuring25Plus);
+		assertEquals(8, analysis.formulaCellsOccuring50Plus);
+		assertEquals(2, analysis.formulaCellsOccuring100Plus);
+		assertEquals(288, analysis.mostFrequentFormulaCount);
+		assertEquals("R[-1]C[0]-R[-3]C[0]", analysis.mostFrequentFormula);
+		assertFalse(analysis.containsMacros);
+		assertEquals(1389, analysis.countIF);
+		assertEquals(45, analysis.countINDEX);
+		assertEquals(24, analysis.countISTEXT);
+		assertEquals(60, analysis.countMATCH);
+		assertEquals(384, analysis.countMIN);
+		assertEquals(780, analysis.countOFFSET);
+		assertEquals(12, analysis.countOR);
+		assertEquals(24, analysis.countROUND);
+		assertEquals(24, analysis.countROWS);
+		assertEquals(981, analysis.countSUM);
+		assertEquals(84, analysis.countSUMIF);
+		// all others are 0
 	}
 	
 	@Test
